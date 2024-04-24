86b9167a4afa6c25e7fc607d2d5d0ad4411c5333
User Kucherenko <raito-werner@yandex.ru>
2024-04-24 21:35:53 +0300

    Созданы сущности Film, User; созданы контроллеры; созданы тесты

--- src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDate.java
@@ -4,9 +4,9 @@ import jakarta.validation.Payload;
 
 import java.lang.annotation.*;
 
-@Target(value=ElementType.FIELD)
-@Retention(value=RetentionPolicy.RUNTIME)
-@Constraint(validatedBy=ReleaseDateValidator.class)
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ReleaseDateValidator.class)
 public @interface ReleaseDate {
 
     String message() default "{ReleaseDate}";
